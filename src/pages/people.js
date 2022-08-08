44703ce2ee2a849a42b119f90a83f00007d2c5a4
pmallab <87636684+pmallab@users.noreply.github.com>
2022-08-08 23:01:27 +0530

    Update people.js

--- src/pages/people.js
+++ src/pages/people.js
@@ -418,6 +418,29 @@ const People = () => {
 <pre></pre>
 
 
+<div class="container">
+<div class="row">
+<div class="col-sm">
+<Img className="Member_5" fixed={data.filemsc_9.childImageSharp.fixed}></Img>
+</div>
+<div class="col-sm">
+<div className= "Phd1_data">
+<h3>Supratim Chatterjee
+<p><small class="text-muted"></small></p></h3>
+<p>Joined this group in August 2022</p>
+<p>Currently continuing Int. MSc(NISER), 7th semester</p>
+<p> Qualified NEST Exam (AIR 511)</p>
+<p>Hometown: Kolkata, West Bengal</p>
+<p>Research field: Synthetic organic chemistry</p>
+<p><a href="mailto: supratim.chatterjee@niser.ac.in"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+<path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
+</svg>&nbsp;supratim.chatterjee@niser.ac.in</a>&emsp;</p>
+</div>
+</div>
+</div>
+</div>
+<pre></pre>
+
 
 
 
