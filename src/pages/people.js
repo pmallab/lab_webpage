88709aae0123173586772bebb1a10f191d6aa5d2
pmallab <87636684+pmallab@users.noreply.github.com>
2022-08-08 23:42:14 +0530

    Update people.js

--- src/pages/people.js
+++ src/pages/people.js
@@ -329,7 +329,7 @@ const People = () => {
 <p> Qualified GATE 2021</p>
 <p>Joined this group in July-2021</p>
 <p>Homeplace: Haldia, West Bengal</p>
-<p>Research field: Currently through course work</p>
+<p>Research field: Visible-light mediated carbon heteroatom bond formation reactions</p>
 <p><a href="mailto: buddhadeb.pal@niser.ac.in"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
 <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
 </svg>&nbsp;buddhadeb.pal@niser.ac.in</a></p>
