c5cfab414e0fedceb0bd827decc9458c81882c87
pmallab <87636684+pmallab@users.noreply.github.com>
2022-08-11 22:35:09 +0530

    Update pmal.js

--- src/pages/pmal.js
+++ src/pages/pmal.js
@@ -125,7 +125,23 @@ const Pmal = () => {
 <div className="Head">
 <h3>Teachings</h3>
 <ul class="List5">
-
+  
+  
+<li>
+<dl class="row">
+<dt class="col-sm-1">2022</dt>
+<dd class="col-sm-9">
+  <dl class="row">
+    <dt class="col-sm-2">August</dt>
+    <dd class="col-sm-8">C203 Reaction Mechanism in Organic Chemistry</dd>
+    </dl>
+    <dl class="row">
+      <dt class="col-sm-2"></dt>
+      <dd class="col-sm-8"></dd>
+      </dl>
+    </dd>
+</dl></li>
+   
 <li>
 <dl class="row">
 <dt class="col-sm-1">2020</dt>
