17c4a0bf30369514d4374423cc3b0c78c0ea1109
pmallab <87636684+pmallab@users.noreply.github.com>
2023-10-03 15:15:40 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,11 +72,11 @@ const Index = () => {
       <h3> News</h3>
       <marquee width="100%" direction="up" scrolldelay="600" >
 
+
+      <p> 25 Sept 2023: Heartiest congratulations to Rosalin for her latest paper in Chem Asian J.</p>
       <p> 05 Jul 2023: Overwhelmed to share our work published in JOC. Tells how to achieve 100%-Z styryl sulfides.</p>
       <p> 03 Jul 2023: Many congratulations to Ashis and Buddhadeb for their latest paper in JOC. Best of luck.</p>
-      <p> 26 May 2023: Congratulations to Dr. Sudip for achieving the HBNI Outstanding Doctoral Student Award 2023 in Chemical Sciences. We are proud of you.</p> 
-      <p> 26 May 2023: Heartiest congratulations to Buddha for his latest article in OL. Excellent beginning</p>
-      
+     
       
      
      
