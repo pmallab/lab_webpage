9f8c50c04fee41dbc4acc295857d80fba564d6a3
pmallab <87636684+pmallab@users.noreply.github.com>
2025-04-19 16:47:51 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,12 +73,13 @@ const Index = () => {
       <marquee width="100%" direction="up" scrolldelay="600" >
 
 
-
+      <p> 19 Apr 2025: Many congratulations to Sathi and Tarun for the latest paper in ACS Sustain. Chem. Eng.</p>
+      <p> 08 Apr 2025: Heartiest congratulations to Tarun for his latest paper in Chem. Eur. J.</p>
       <p> 25 Mar 2025: Many congratulations to Tarun for his latest paper in ACS AMI.</p>
       <p> 04 Mar 2025: Heartiest congratulations to Anupam and Pravat for their latest publication in ACS Energy Lett.</p>
-      <p> 31 Jan 2025: Many congratulations to Pravat for his latest AJOC.</p>
-      <p> 09 Jan 2025: Exciting! We started the new year with Org. Lett. Heartiest congratulations to Buddha.</p> 
-      <p> 06 Jan 2025: Many congratulations to Ramen, joined the group for his PhD thesis.</p>
+      
+      
+      
       
       
       
