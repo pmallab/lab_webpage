1438e6b8cd1b3929e7c73111c32e73174a2464bb
pmallab <87636684+pmallab@users.noreply.github.com>
2026-01-20 10:38:44 +0530

    Update publications.js

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -18,6 +18,9 @@ const Publications = () => {
       <small class="text-muted"> In 2025</small>
     </h2>
 
+  <li><p> P. Nayek, R. Guchait, T. N. Deshpande, B. Debnath, R. Bhanja, and P. Mal, Solvent Controlled NBS-Driven Divergent Bromo-Lactonization of Ortho-Styryl Benzamides,  Eur. J. Org. Chem., 2026,
+  <Link className="Pub1link" to="https://doi.org/10.1002/ejoc.202501176" target="_blank"> (doi.org/10.1002/ejoc.202501176)</Link></p></li>
+    
   <li><p> P. Nayek, C. Biswal, S. Maity and P. Mal, Organic Halogenation of Alkynes with Inorganic Halides Using Perylenediimide as Visible-Light Photocatalyst,  Adv. Synth. Catal., 2025,
   <Link className="Pub1link" to="https://doi.org/10.1002/adsc.70245" target="_blank"> (doi.org/10.1002/adsc.70245)</Link></p></li>
     
